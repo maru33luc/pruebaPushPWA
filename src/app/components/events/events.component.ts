db5faf4dba5c634dd8e89578dc7d6d78938a1155
maru33luc <maru33luc@gmail.com>
2024-04-01 12:36:54 -0300

    [STYLES] Events gsap

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -31,29 +31,50 @@ export class EventsComponent implements OnInit {
     const photos = gsap.utils.toArray<HTMLElement>('.desktopPhoto:not(:first-child)');
     const allPhotos = gsap.utils.toArray<HTMLElement>('.desktopPhoto');
 
-    gsap.set(photos, { yPercent: 100 });
+    gsap.set(photos, { yPercent: 101 });
 
-    ScrollTrigger.create({
-      trigger: '.gallery',
-      start: 'top top',
-      end: 'bottom bottom',
-      pin: '.right'
-    });
+    // create
+let mm = gsap.matchMedia();
 
-    details.forEach((detail, index) => {
-      let headline = detail.querySelector('h1');
-      let animation = gsap.timeline()
-        .to(photos[index], { yPercent: 0, duration: 1.5,ease: 'power2.inOut'})
-        .set(allPhotos[index], { autoAlpha: 0 });
+// add a media query. When it matches, the associated function will run
+mm.add("(min-width: 800px)", () => {
 
-      ScrollTrigger.create({
-        trigger: headline,
-        start: 'top 60%',
-        end: 'top 50%',
-        animation: animation,
-        scrub: true,
-        markers: false
-      })
-    });
+  // this setup code only runs when viewport is at least 600px wide
+  console.log("desktop")
+	
+  ScrollTrigger.create({
+	trigger:".gallery",
+	start:"top top",
+	end:"bottom bottom",
+	pin:".right",
+  
+})
+
+//create scrolltrigger for each details section
+//trigger photo animation when headline of each details section 
+//reaches 80% of window height
+details.forEach((detail, index)=> {
+
+	let headline = detail.querySelector("h1")
+	let animation = gsap.timeline()
+	   .to(photos[index], {yPercent:0})
+	   .set(allPhotos[index], {autoAlpha:0})
+	ScrollTrigger.create({
+		trigger:headline,
+		start:"top 80%",
+		end:"top 50%",
+		animation:animation,
+		scrub:true,
+		markers:false
+	})
+})
+	
+	
+  
+  return () => { // optional
+    // custom cleanup code here (runs when it STOPS matching)
+	  console.log("mobile")
+  };
+});
   }
 }
