bdfc14815d923854b5753bd171c3184dcc193cc5
maru33luc <maru33luc@gmail.com>
2024-04-02 18:50:29 -0300

    [FIX] Dom Errors news comp

--- src/app/components/news-slider/news-slider.component.ts
+++ src/app/components/news-slider/news-slider.component.ts
@@ -10,61 +10,64 @@ import { SpacerTitleComponent } from '../spacer-title/spacer-title.component';
   templateUrl: './news-slider.component.html',
   styleUrl: './news-slider.component.css'
 })
-export class NewsSliderComponent {
+export class NewsSliderComponent  {
   
   title: string = "Últimas noticias";
   marginTop: number = 0;
   paddingTop: number = 100;
 
-  ngOnInit(): void {
-    gsap.registerPlugin(ScrollTrigger);
+  ngAfterViewInit(): void {
+    // Verificamos si document está definido para evitar errores en entornos no de navegador
+    if (typeof document !== 'undefined') {
+      gsap.registerPlugin(ScrollTrigger);
 
-    const container = document.querySelector(".container");
-    const sections = gsap.utils.toArray(".container section");
-    const texts = gsap.utils.toArray(".anim");
-    const mask = document.querySelector(".mask");
+      const container = document.querySelector(".container");
+      const sections = gsap.utils.toArray(".container section");
+      const texts = gsap.utils.toArray(".anim");
+      const mask = document.querySelector(".mask");
 
-    let scrollTween = gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".container",
-        pin: true,
-        scrub: 1,
-        end: "+=3000",
-        markers: false,
-      }
-    });
-
-    console.log(1 / (sections.length - 1));
-
-    gsap.to(mask, {
-      width: "100%",
-      scrollTrigger: {
-        trigger: ".wrapper",
-        start: "top left",
-        scrub: 1
-      }
-    });
-
-    sections.forEach((section) => {
-      let text = (section as HTMLElement).querySelectorAll(".anim");
-      
-      if (text.length === 0) return;
-
-      gsap.from(text, {
-        y: -130,
-        opacity: 0,
-        duration: 2,
-        ease: "elastic",
-        stagger: 0.1,
+      let scrollTween = gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: "none",
         scrollTrigger: {
-          trigger: section as HTMLElement,
-          containerAnimation: scrollTween,
-          start: "left center",
-          markers: false
+          trigger: ".container",
+          pin: true,
+          scrub: 1,
+          end: "+=3000",
+          markers: false,
         }
       });
-    });
+
+      console.log(1 / (sections.length - 1));
+
+      gsap.to(mask, {
+        width: "100%",
+        scrollTrigger: {
+          trigger: ".wrapper",
+          start: "top left",
+          scrub: 1
+        }
+      });
+
+      sections.forEach((section) => {
+        let text = (section as HTMLElement).querySelectorAll(".anim");
+        
+        if (text.length === 0) return;
+
+        gsap.from(text, {
+          y: -130,
+          opacity: 0,
+          duration: 2,
+          ease: "elastic",
+          stagger: 0.1,
+          scrollTrigger: {
+            trigger: section as HTMLElement,
+            containerAnimation: scrollTween,
+            start: "left center",
+            markers: false
+          }
+        });
+      });
+    }
   }
 }
