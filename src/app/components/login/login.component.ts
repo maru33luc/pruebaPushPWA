5d9df3c372539834d011973052dafa875224fb62
maru33luc <maru33luc@gmail.com>
2024-04-03 12:14:17 -0300

    [FEAT] Login form inputs

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import swal from 'sweetalert';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +16,14 @@ export class LoginComponent {
 
   loginForm: FormGroup
 
-  constructor(private fb: FormBuilder) {
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private loginService: LoginService) {
     this.loginForm = this.fb.group({
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
+      repassword: ['', Validators.required]
     })
   }
 
@@ -25,6 +32,64 @@ export class LoginComponent {
   }
   send() {
     console.log(this.loginForm.value);
+    swal("Hello world!");
+    const validPass = this.validatePassword();
+    if (validPass) {
+      this.loginService.register(this.loginForm.value.username, this.loginForm.value.email, this.loginForm.value.password)
+    } else {
+      swal("Las contraseñas no coinciden");
+    }
   }
 
+  validatePassword() {  
+    const password = this.loginForm.get('password')?.value; // Add null check
+    const repassword = this.loginForm.get('repassword')?.value; // Add null check
+    return password === repassword ? true : false
+  }
+
+
+  async login() {
+    if (this.loginForm.invalid) {
+        this.loginForm.markAllAsTouched();
+    } else {
+        try {
+            
+            const res = await this.loginService.login(
+                this.loginForm.value.email,
+                this.loginForm.value.password
+            );
+            if(res){
+                this.router.navigate(['']);
+            }
+        } catch (e) {
+            console.log(e);
+            swal("No se pudo iniciar sesión");
+        }
+    }
+}
+
+async register () {
+    if (this.loginForm.invalid) {
+        this.loginForm.markAllAsTouched();
+    } else {
+        const validPass = this.validatePassword();
+        if (validPass) {
+          try {
+            const res = await this.loginService.register(
+              this.loginForm.value.username,
+              this.loginForm.value.email,
+              this.loginForm.value.password
+            );
+            if (res !== undefined && res !== null) {
+              this.router.navigate(['']);
+            }
+          } catch (e) {
+            console.log(e);
+            swal("No se pudo registrar");
+          }
+        } else {
+            swal("Las contraseñas no coinciden");
+        }
+    }
+  }
 }
