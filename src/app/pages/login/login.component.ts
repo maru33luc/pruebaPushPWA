226fcd65f7d9998b79659c7a2330d41a5e3346ae
maru33luc <maru33luc@gmail.com>
2024-04-03 02:27:25 -0300

    [FEAT] Login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../../components/login/login.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [LoginComponent, HeaderComponent],
+  imports: [LoginComponent, HeaderComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
